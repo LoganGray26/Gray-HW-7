43b2181b856609e7e81344e2bac31ad341971a55
logan Gray <ld.gray@setonhill.edu>
2020-05-03 21:24:31 -0400

    Ready

---
+++ Gray HW #7/GPSTBViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GPSTBViewController.swift
+//  Gray HW #7
+//
+//  Created by logan gray on 5/3/20.
+//  Copyright © 2020 logan gray. All rights reserved.
+//
+
+import UIKit
+
+class GPSTBViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
